8f8705969ffb4f480f455d926d7fefa86cf6b06f
Justin031806 <justinralston@unomaha.edu>
2025-04-16 15:31:37 +0000

    done

--- main.py
+++ main.py
@@ -2,3 +2,32 @@
 #Name:
 #Date:
 #Assignment:
+
+
+
+import coffee
+import pandas
+import matplotlib as plt
+
+coffee=coffee.get_coffee()
+
+#print(coffee[0]["Data"]["Scores"]["Total"])
+years=[]
+scores=[]
+for bean in coffee:
+    year=bean["Year"]
+    score=bean["Data"]["Scores"]["Total"]
+    if score !=0:
+      years.append(year)
+      scores.append(score)
+    #print(year,score)
+
+    df=pandas.DataFrame([{"Year":years,
+                         "Score":scores}])
+    
+    print(df)
+    df.plot(kind='scatter',x='Year',y='Score')
+   
+    plt.savefig("Output")
+
+
